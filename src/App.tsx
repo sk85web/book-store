6a059f6b35286180d790ee9f3c6dd352fb70b35d
Sergey <sk85post@gmail.com>
2024-05-21 14:35:35 +0300

    init: start project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Home/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
